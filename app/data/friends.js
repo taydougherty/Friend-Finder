553c403ce1cc00d1728174ce491f20aedff06434
Taylor Dougherty <45830707+taydougherty@users.noreply.github.com>
2019-06-17 22:00:21 -0700

    added routing and html

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendArray = [
+    {
+      "name": "Taylor",
+      "photo": "",
+      "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1  
+      ]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendArray;
