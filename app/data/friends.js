8b9ed2da3ea55532be69d5e5acfe36029378f690
Taylor Dougherty <45830707+taydougherty@users.noreply.github.com>
2019-06-19 15:11:40 -0700

    added jquery for survey page

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,3 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
-
 var friendArray = [
     {
       "name": "Taylor",
@@ -24,5 +17,4 @@ var friendArray = [
     }
   ];
   
-  // Note how we export the array. This makes it accessible to other files using require.
   module.exports = friendArray;
