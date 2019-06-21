3b0d3940ad9ba75a9d05a4e371887113c5f4dbd9
Taylor Dougherty <45830707+taydougherty@users.noreply.github.com>
2019-06-21 11:02:08 -0700

    updated image links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendArray = [
     {
       name: "Vienna",
-      photo: "https://images.app.goo.gl/JFEkLAx1ViZTS44H6",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwitqbjpkPviAhXT6Z4KHSRoDLUQjRx6BAgBEAU&url=https%3A%2F%2Fwww.toopics.com%2Fviennaonfifth%2F&psig=AOvVaw3HeSG6AUGDWVmH-L5_31XX&ust=1561225628186401",
       scores: [
         5,
         1,
@@ -17,7 +17,7 @@ var friendArray = [
     },
     {
         name: "Corky",
-        photo: "https://images.app.goo.gl/qFzTybsXHLrZVoEMA",
+        photo: "https://scontent-lga3-1.cdninstagram.com/vp/daaae055d4768d3eb607f464ee3e075b/5D0E9837/t51.2885-15/sh0.08/e35/c0.180.1440.1440/s640x640/52891221_379488919553907_8123796071745555731_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com",
         scores: [
           5,
           2,
