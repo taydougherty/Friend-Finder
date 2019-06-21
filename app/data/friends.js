676e265a7316c1d39f2fade862168cb69f70dbac
Taylor Dougherty <45830707+taydougherty@users.noreply.github.com>
2019-06-21 11:28:30 -0700

    added images and data to friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendArray = [
     {
-      name: "Vienna",
-      photo: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      name: "Max",
+      photo: "https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg",
       scores: [
         5,
         1,
@@ -16,8 +16,8 @@ var friendArray = [
       ]
     },
     {
-        name: "Corky",
-        photo: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        name: "Princess",
+        photo: "https://nevadahumanesociety.org/wp-content/uploads/2018/08/NHS-Slider-03-1024x731.jpg",
         scores: [
           5,
           2,
@@ -29,6 +29,38 @@ var friendArray = [
           1,
           1,
           1  
+        ]
+      },
+      {
+        name: "Spot",
+        photo: "https://timedotcom.files.wordpress.com/2016/03/wellbeing-at-work.jpg?quality=85",
+        scores: [
+          1,
+          2,
+          1,
+          3,
+          2,
+          5,
+          5,
+          1,
+          1,
+          5  
+        ]
+      },
+      {
+        name: "Rover",
+        photo: "https://e3.365dm.com/19/06/1600x900/skynews-sad-dogs-evolve-study_4696589.jpg?20190617211718",
+        scores: [
+          1,
+          2,
+          3,
+          4,
+          5,
+          1,
+          2,
+          3,
+          4,
+          5  
         ]
       },
   ];
