bcb49f987b2c36ede573fd7df3010bc52acdb893
Taylor Dougherty <45830707+taydougherty@users.noreply.github.com>
2019-06-20 19:35:26 -0700

    edited routes

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
